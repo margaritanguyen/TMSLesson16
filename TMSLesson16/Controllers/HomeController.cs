a6c400d2a29c3c875f469715830848fcefafae4d
Margarita <nguyenmargarita.w@gmail.com>
2023-03-23 15:07:08 +0300

    init commit

---
+++ TMSLesson16/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TMSLesson16.Models;
+
+namespace TMSLesson16.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username))
+            {
+                return PartialView("PartialView");
+            }
+
+            if (username.Equals("tmsuser") && password.Equals("123"))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return NotFound("Login Failed");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
